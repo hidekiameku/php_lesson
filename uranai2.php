d3b32495ee0d823af6da4d508714119364fed660
HidekiAmeku <hidekiameku@gmail.com>
2016-07-14 03:42:30 +0900

    edit programing

--- uranai2.php
+++ uranai2.php
@@ -1,8 +1,27 @@
 <?php
-if(is_numeric($_GET["age"]) == true){
-  echo"あなたの年齢は:";
-  echo htmlspecialchars($_GET["age"]);
-  echo "才です";
-}else {
-  echo "入力された値は数字ではありません";
+if(is_numeric($_GET["age"]) == false){
+  echo "年齢は数字で教えてください！";
+  exit();
 }
+
+$uranai_message = [
+                    "ちょっとあなたの年齢は対象年齢に含まれていません。。。",
+                    "10代のあなたには、PHPを御守り代わりに勉強するのがいいみたい",
+                    "20代のあなたには、趣味でPHPを使うと良いみたい",
+                    "30代のあなたには、ビジネススキルとしてPHPを勉強すると良いみたい",
+                    "40代のあなたには、PHPは新たなフロンティアとなるでしょう",
+                    ];
+
+if ($_GET["age"] >= 50 || $_GET["age"] < 10 ) {
+  $num = 0;
+}else if ($_GET["age"] >= 40 ) {
+  $num = 4;
+}else if ($_GET["age"] >= 30 ) {
+  $num = 3;
+}else if ($_GET["age"] >= 20 ) {
+  $num = 2;
+}else {
+  $num = 1;
+}
+
+echo "$uranai_message[$num]";
